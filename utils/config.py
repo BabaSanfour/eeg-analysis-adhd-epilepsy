12bbd2a7b18bc97e6b88cff07ef9cf63eb7f56b4
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2025-04-08 17:39:12 -0400

    feat: update data directory paths for different user environments and increment subject count

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,12 @@ import numpy as np
 
 user = os.path.expanduser('~')
 # Path to the directory where the data is stored
-data_dir = os.path.join(user, 'Projects/data/EEG_psychostimulant_data/EEG_psychostimulants_2025-02')
+if user == '/home/hamza97':
+    data_dir = os.path.join(user, 'scratch/data/EEG_psychostimulants_2025-02')
+elif user == '/Users/hamzaabdelhedi':
+    data_dir = os.path.join(user, 'Projects/data/EEG_psychostimulant_data/EEG_psychostimulants_2025-02') 
+embeddings_dir = os.path.join(data_dir, 'embeddings')
+results_dir = os.path.join(data_dir, 'results')
 source_dirs = {"control": "Controls", "patients": "patients", }
 csv_dir = os.path.join(data_dir, 'csv')
 bids_dir = os.path.join(data_dir, 'BIDS')
@@ -11,8 +16,7 @@ derivatives_dir = os.path.join(data_dir, 'derivatives')
 sensors_to_keep = ["Fp1", "Fp2", "F7", "F3", "Fz", "F4", "F8", "A1", "T3", "C3", "Cz",
                 "C4", "T4", "A2", "T5", "P3", "Pz", "P4", "T6", "O1", "O2"]
 
-results_dir = os.path.join(data_dir, 'results')
-n_subjects = 252
+n_subjects = 253
 
 
 # Mapping to compute psychostimulant category based on description
