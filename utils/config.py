488eac4ded329027d4730ff5b98f0c9b0cab8d98
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2025-05-09 11:40:48 -0400

    fix: update data directory paths and mapping for psychostimulant categories

--- utils/config.py
+++ utils/config.py
@@ -4,12 +4,11 @@ import numpy as np
 user = os.path.expanduser('~')
 # Path to the directory where the data is stored
 if user == '/home/hamza97':
-    data_dir = os.path.join(user, 'scratch/data/EEG_psychostimulants_2025-02')
+    data_dir = os.path.join(user, 'projects/rrg-shahabkb/EEG_ADHD_epilepsy_psychostimulants')
 elif user == '/Users/hamzaabdelhedi':
     data_dir = os.path.join(user, 'Projects/data/EEG_psychostimulant_data/EEG_psychostimulants_2025-02') 
 embeddings_dir = os.path.join(data_dir, 'embeddings')
 results_dir = os.path.join(data_dir, 'results')
-source_dirs = {"control": "Controls", "patients": "patients", }
 csv_dir = os.path.join(data_dir, 'csv')
 bids_dir = os.path.join(data_dir, 'BIDS')
 derivatives_dir = os.path.join(data_dir, 'derivatives')
@@ -21,7 +20,7 @@ n_subjects = 253
 
 # Mapping to compute psychostimulant category based on description
 MAPPING_PSYCHOSTIMULANT = {
-    np.nan: 0,
+    'no psychostimulants': 0,
     'Lisdexamfetamine': 1,
     'Lisdexamfetamine (d/c)': 1,
     'Methylphenidate (d/c)': 2,
