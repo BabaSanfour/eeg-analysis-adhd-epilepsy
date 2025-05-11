2e777f475b2e08c33209b16c52a3aa2a6b46040f
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2025-05-10 21:31:26 -0400

    feat: add embedding processing script and refactor EEG data handling in reve_model.py

---
+++ dl_pipelines/embed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --account=def-kjerbi
+#SBATCH --mail-user=hamza.abdelhedi@umontreal.ca
+#SBATCH --mail-type=ALL
+#SBATCH --time=2:40:00
+#SBATCH --gpus=a100_1g.5gb:1
+#SBATCH --mem=20G
+#SBATCH --output=embed.out
+#SBATCH --error=embed.err
+#SBATCH --job-name=embed
+
+
+source /home/hamza97/venv_eeg_psychostim/bin/activate
+
+python reve_model.py --derivatives_dir "/home/hamza97/projects/rrg-shahabkb/cocolab_data/EEG_ADHD_epilepsy_psychostimulants/derivatives/embedding_processing" --embeddings_dir "/home/hamza97/projects/rrg-shahabkb/cocolab_data/EEG_ADHD_epilepsy_psychostimulants/embeddings" --n_subjects 1013 --start_subject 1 --segment_duration 10 --z_score --z_score_axis 1
+
+
+python reve_model.py --derivatives_dir "/home/hamza97/projects/rrg-shahabkb/cocolab_data/EEG_ADHD_epilepsy_psychostimulants/derivatives/embedding_processing" --embeddings_dir "/home/hamza97/projects/rrg-shahabkb/cocolab_data/EEG_ADHD_epilepsy_psychostimulants/embeddings" --n_subjects 1013 --start_subject 1 --segment_duration 10 --z_score --z_score_axis 0
